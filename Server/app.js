c803ac308d75cec589f05d5f671b30d622a6b252
Aakash <aakashmeshram@outlook.com>
2020-09-14 19:24:23 +0530

    Updated for heroku deployment

--- Server/app.js
+++ Server/app.js
@@ -49,7 +49,7 @@ app.use('/api/v1/note', noteRouter)
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/build'))
-  app.get('*', req, res =>
+  app.get('*', (req, res) =>
     res.sendFile(path.resolve('../client/build/index.html'))
   )
 }
